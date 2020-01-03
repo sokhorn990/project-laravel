4b339206ea7d6947569806229122f7949e82d10c
sokhorn990 <sono.sunowner@gmail.com>
2020-01-03 20:38:48 +0700

    project laravel

---
+++ web2020a/resources/views/pages/our_action.blade.php
@@ -0,0 +1,6 @@
+
+<h1>Our action in Cambodia</h1>
+<p>Passerelles numériques Cambodia was the first project to be launched in 2005, in
+Phnom Penh. PNC offers two IT training programs in System and Network
+Administration (SNA) and Web Programming (WEP), based on a holistic educative
+approach including technical and soft skills, and a personal development program</p>
